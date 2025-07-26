3805286d0dc3f57bfecbc9bc182dc9f6d31e8617
PZ2810 <121484876+PZ2810@users.noreply.github.com>
2025-07-27 01:59:00 +0530

    Add files via upload

---
+++ Delta/JS/app.js
@@ -0,0 +1,19 @@
+// console.log("hello world");
+// console.log("Apna college");
+// let a= 10;
+// let b = 5;
+// console.log("sum is :", a+b);
+let pencilPrice=150;
+let erasorPrice =15;
+// let output = "The total price is :" + (pencilPrice+erasorPrice) +" Rupees.";
+//let output=`The total price is : ${pencilPrice+erasorPrice} Rupees`
+let a= 5;
+let b=2;
+// console.log(a+b);
+// console.log(a-b);
+// console.log(a*b);
+// console.log(a/b);
+// console.log(a%b);
+
+console.log(a++)//post increment
+console.log(++a);//pre increment
