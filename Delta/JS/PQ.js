dd65e8ea7b9df0182b5820e16c0a59a5df096602
PZ2810 <121484876+PZ2810@users.noreply.github.com>
2025-07-27 16:32:05 +0530

    Add files via upload

---
+++ Delta/JS/PQ.js
@@ -0,0 +1,12 @@
+let color= "red";
+//Traffic Light System
+if (color==="red"){
+    console.log("stop")
+}
+if(color==="yellow"){
+    console.log("slow down")
+
+}
+if (color==="green"){
+    console.log("GO")
+}
--- Delta/JS/app.js
+++ Delta/JS/app.js
@@ -2,18 +2,41 @@
 // console.log("Apna college");
 // let a= 10;
 // let b = 5;
-// console.log("sum is :", a+b);
-let pencilPrice=150;
-let erasorPrice =15;
-// let output = "The total price is :" + (pencilPrice+erasorPrice) +" Rupees.";
-//let output=`The total price is : ${pencilPrice+erasorPrice} Rupees`
-let a= 5;
-let b=2;
-// console.log(a+b);
-// console.log(a-b);
-// console.log(a*b);
-// console.log(a/b);
-// console.log(a%b);
+// // console.log("sum is :", a+b);
+// let pencilPrice=150;
+// let erasorPrice =15;
+// // let output = "The total price is :" + (pencilPrice+erasorPrice) +" Rupees.";
+// //let output=`The total price is : ${pencilPrice+erasorPrice} Rupees`
+// let a= 5;
+// let b=2;
+// // console.log(a+b);
+// // console.log(a-b);
+// // console.log(a*b);
+// // console.log(a/b);
+// // console.log(a%b);
 
-console.log(a++)//post increment
-console.log(++a);//pre increment
+// console.log(a++)//post increment
+// console.log(++a);//pre increment
+
+// conditional statements
+
+// console.log("before my if statement");
+// let age =23;
+// if(age>=18){
+//     console.log("you can vote");
+//     console.log("you can drive")
+// }
+// console.log("after my if statement");
+
+// let firstName="Pranav";
+// if(firstName=="Pranav"){
+//     console.log(`welcome${firstName}`)
+// }
+
+let age =14;
+if(age>=18){
+    console.log("you can vote");
+
+}else if(age<18){
+    console.log("you cannot vote");
+}
