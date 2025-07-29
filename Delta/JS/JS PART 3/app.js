7d768ddb149f712cacc4cf008d09f3ba6a34f17d
PZ2810 <121484876+PZ2810@users.noreply.github.com>
2025-07-29 23:11:10 +0530

    Add files via upload

--- Delta/JS/JS PART 3/app.js
+++ Delta/JS/JS PART 3/app.js
@@ -1,4 +1,4 @@
-//let msg ="  he   llo   "
+// let msg ="  he   llo   "
 
 // let password =prompt("set your password");
 // console.log(password .trim());
@@ -6,7 +6,49 @@
 // let name ="pranav";
 //  let upper =name.toUpperCase();
 // console.log(upper);
-//smae toLowerCase()
-//slicing
-//str.replace
-//str.repeat 
+// //smae toLowerCase()
+// ////slicing
+// //str.replace
+// //str.repeat 
+
+let students= ["aman", "pranav","mubashira"] ;
+console.log(students);
+
+//students.indexOf("pranav");
+console.log(students.indexOf("pranav"));
+//students.indexOf("Pranav");
+console.log(students.indexOf("Pranav"));
+//students.includes("pranav");
+console.log(students.includes("pranav"));
+//students.includes("Pranav");
+console.log(students.includes("Pranav"));
+
+//concat  merge 2 arrays
+let first =["Red","yellow","blue"];
+let second =["Orange", "green","voilet"]
+console.log(second.concat(first));
+
+//reverse an array
+console.log(second.reverse());
+ 
+//Array Methods
+//slice
+let colors=second.concat(first);
+console.log(colors);
+//colors.slice();
+console.log(colors.slice());
+//colors.slice(2);
+console.log(colors.slice(2));
+//colors.slice(2,3);
+console.log(colors.slice(2,3));
+//colors.slice(-2);
+console.log(colors.slice(-2));
+
+//splice removes/replaces/add elements in place
+let color=["red","yellow","blue","orange","pink","white"];
+color.splice(4);
+console.log(color);
+color.splice(0,1);
+console.log(color);
+color.splice(0,1,"black","grey");
+console.log(color);
