154b219cdb42ab4e7abf3eca5fd11284b83066a4
PZ2810 <121484876+PZ2810@users.noreply.github.com>
2025-08-01 23:56:18 +0530

    Add files via upload

--- Delta/JS/JS PART 3/PQ.js
+++ Delta/JS/JS PART 3/PQ.js
@@ -1,14 +1,17 @@
-// //qs1
-// // let msg = "help!";
-// // console.log(msg.trim(),msg.toUpperCase());
-// let name="ApnaCollege";
-// console.log(name.slice(4,9));
-// console.log(name.indexOf("na"));
-// console.log(name.replace("Apna", "Our"))
+// // //qs1
+// // // let msg = "help!";
+// // // console.log(msg.trim(),msg.toUpperCase());
+// // let name="ApnaCollege";
+// // console.log(name.slice(4,9));
+// // console.log(name.indexOf("na"));
+// // console.log(name.replace("Apna", "Our"))
 
- let months=["january","july","march","august"]
- months.splice(0,2,"july","june");
- console.log(months);
+//  let months=["january","july","march","august"]
+//  months.splice(0,2,"july","june");
+//  console.log(months);
 
- let lang=["c","c++","html","javascript","python","java","c#","sql"]
- console.log(lang.reverse().indexOf("javascript"));
+//  let lang=["c","c++","html","javascript","python","java","c#","sql"]
+//  console.log(lang.reverse().indexOf("javascript"));  
+
+let tictac=[['X',null,'O'],[null,'X',null],['O',null,'X']];
+console.log(tictac);
--- Delta/JS/JS PART 3/app.js
+++ Delta/JS/JS PART 3/app.js
@@ -1,60 +1,63 @@
- // let msg ="  he   llo   "
+// //  // let msg ="  he   llo   "
 
-// let password =prompt("set your password");
-// console.log(password .trim());
+// // // let password =prompt("set your password");
+// // // console.log(password .trim());
 
-// let name ="pranav";
-//  let upper =name.toUpperCase();
-// console.log(upper);
-// //smae toLowerCase()
-// ////slicing
-// //str.replace
-// //str.repeat 
+// // // let name ="pranav";
+// // //  let upper =name.toUpperCase();
+// // // console.log(upper);
+// // // //smae toLowerCase()
+// // // ////slicing
+// // // //str.replace
+// // // //str.repeat 
 
-let students= ["aman", "pranav","mubashira"] ;
-console.log(students);
+// // let students= ["aman", "pranav","mubashira"] ;
+// // console.log(students);
 
-//students.indexOf("pranav");
-console.log(students.indexOf("pranav"));
-//students.indexOf("Pranav");
-console.log(students.indexOf("Pranav"));
-//students.includes("pranav");
-console.log(students.includes("pranav"));
-//students.includes("Pranav");
-console.log(students.includes("Pranav"));
+// // //students.indexOf("pranav");
+// // console.log(students.indexOf("pranav"));
+// // //students.indexOf("Pranav");
+// // console.log(students.indexOf("Pranav"));
+// // //students.includes("pranav");
+// // console.log(students.includes("pranav"));
+// // //students.includes("Pranav");
+// // console.log(students.includes("Pranav"));
 
-//concat  merge 2 arrays
-let first =["Red","yellow","blue"];
-let second =["Orange", "green","voilet"]
-console.log(second.concat(first));
+// // //concat  merge 2 arrays
+// // let first =["Red","yellow","blue"];
+// // let second =["Orange", "green","voilet"]
+// // console.log(second.concat(first));
 
-//reverse an array
-console.log(second.reverse());
+// // //reverse an array
+// // console.log(second.reverse());
  
-//Array Methods
-//slice
-let colors=second.concat(first);
-console.log(colors);
-//colors.slice();
-console.log(colors.slice());
-//colors.slice(2);
-console.log(colors.slice(2));
-//colors.slice(2,3);
-console.log(colors.slice(2,3));
-//colors.slice(-2);
-console.log(colors.slice(-2));
+// // //Array Methods
+// // //slice
+// // let colors=second.concat(first);
+// // console.log(colors);
+// // //colors.slice();
+// // console.log(colors.slice());
+// // //colors.slice(2);
+// // console.log(colors.slice(2));
+// // //colors.slice(2,3);
+// // console.log(colors.slice(2,3));
+// // //colors.slice(-2);
+// // console.log(colors.slice(-2));
 
-//splice removes/replaces/add elements in place
-let color=["red","yellow","blue","orange","pink","white"];
-color.splice(4);
-console.log(color);
-color.splice(0,1);
-console.log(color);
-color.splice(0,1,"black","grey");
-console.log(color);
+// // //splice removes/replaces/add elements in place
+// // let color=["red","yellow","blue","orange","pink","white"];
+// // color.splice(4);
+// // console.log(color);
+// // color.splice(0,1);
+// // console.log(color);
+// // color.splice(0,1,"black","grey");
+// // console.log(color);
 
-//sort an array
+// // //sort an array
 
-let squares=[100,81,64,49,36,25,16,4,2,1];
-//squares.sort()
-console.log(squares.sort());
+// // let squares=[100,81,64,49,36,25,16,4,2,1];
+// // //squares.sort()
+// // console.log(squares.sort());
+
+// let nums=[[1,2],[3,4],[5,6]];
+//   console.log(nums);
