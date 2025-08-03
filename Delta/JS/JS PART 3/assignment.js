8532f66c9acbfa0b3c1f9e4b8cf10fa263485fd8
PZ2810 <121484876+PZ2810@users.noreply.github.com>
2025-08-03 23:00:19 +0530

    Add files via upload

---
+++ Delta/JS/JS PART 3/assignment.js
@@ -0,0 +1,28 @@
+//Qs 1
+// let arr=[7,9,0,-2];
+// let n =3;
+// let ans =arr.slice(0,n);
+// console.log(ans);
+
+// //Qs 2
+// console.log(arr.slice(arr.length-n))
+
+// //Qs 3
+// let str =prompt("please enter a string");
+
+// if (str.length ==0){
+//     console.log("String is empty")
+
+// }else{
+//     console.log(str);
+// }
+
+//Qs 4
+
+let st ="PrAnAv ZhAwAr";
+let idx =2;
+if (st[idx]==st[idx].toLowercase){
+    console.log("character is  toLowercase")
+}else{
+    console.log("not lower")
+}
