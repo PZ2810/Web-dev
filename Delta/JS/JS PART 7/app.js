f43db3394f328cb885b48f604b5a93933733b008
PZ2810 <121484876+PZ2810@users.noreply.github.com>
2025-08-09 23:59:37 +0530

    Add files via upload

---
+++ Delta/JS/JS PART 7/app.js
@@ -0,0 +1,23 @@
+// set timeout (Print after time single time)
+
+// console.log("hi there");
+
+//     setTimeout(  ()=>{
+//         console.log("Pranav")
+
+//     },4000)
+// console.log("welcome");
+
+// Set Interval()   (print contiously after )
+
+    // setInterval( ()=>{
+    //     console.log("Pranav Zhawar")
+
+    // },2000);
+ 
+    // to stop assign it to id 
+    //and then use clearid
+
+
+  const sq=  (n)=>n*n;
+   console.log (sq(2));
