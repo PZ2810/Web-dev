7f3086815975cf530ce78fb24982a5fc0b2f6ef5
PZ2810 <121484876+PZ2810@users.noreply.github.com>
2025-08-11 23:33:48 +0530

    Add files via upload

---
+++ Delta/JS/JS PART 8/PQ.js
@@ -0,0 +1,21 @@
+// //max num
+// let nums=[10,20,30,40,5];
+
+// let ans =nums.every((el)=> el%10==0);
+
+// console.log(ans);
+
+// //min num
+// function getMin(nums){
+//     let min=nums.reduce((min,el)=>{
+// if(min<el){
+//     return min;
+
+// }else{
+//     return el;
+// }
+// });
+// return min;
+// }
+
+// //console.log(min)
---
+++ Delta/JS/JS PART 8/app.js
@@ -0,0 +1,5 @@
+// function sum(a,b=30){
+//     return a+b;
+// }
+// console.log(sum(3));
+
