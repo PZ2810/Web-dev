cb61b81f8fbb78be49f681417f32449a2ab7d422
PZ2810 <121484876+PZ2810@users.noreply.github.com>
2025-08-12 23:39:46 +0530

    Add files via upload

--- Delta/JS/JS PART 8/app.js
+++ Delta/JS/JS PART 8/app.js
@@ -3,3 +3,26 @@
 // }
 // console.log(sum(3));
 
+// spread
+// const data={
+//     email:"ironman@gmail.com",
+//     password:"abcd",
+// };
+
+// const dataCopy={...data,id:123,country:"India"};
+
+// let arr=[1,2,3,4,5];
+// let obj1 ={...arr};
+
+//Rest
+ 
+// function sum(...args){
+//     //arguments
+//     for (let i=0;i<args.length;i++){
+//         console.log("you gave us :" ,args[i]);
+//     }
+// }
+
+ function sum(...args){
+    return args.reduce((sum,el)=>sum+el);
+ }
