54bfd348b9ef658732667f130eefa8fa5d8e3448
PZ2810 <121484876+PZ2810@users.noreply.github.com>
2025-08-29 23:46:19 +0530

    Add files via upload

--- Final/JS DOM-20250825T210025Z-1-001/JS DOM/app.js
+++ Final/JS DOM-20250825T210025Z-1-001/JS DOM/app.js
@@ -11,4 +11,10 @@
 
 // console.dir(document.querySelector(".oldImg"))
 
-console.dir(document.querySelectorAll("div a"));
+//console.dir(document.querySelectorAll("div a"));
+
+let links = document.querySelectorAll(".box a");
+
+for (let i = 0; i < links.length; i++) {
+    links[i].style.color ="yellow";
+}
