39c3ca73b7e2905a437ec7276f848828eab3a82b
PZ2810 <121484876+PZ2810@users.noreply.github.com>
2025-08-30 02:07:35 +0530

    Add files via upload

--- Final/JS DOM-20250825T210025Z-1-001/JS DOM/app.js
+++ Final/JS DOM-20250825T210025Z-1-001/JS DOM/app.js
@@ -11,10 +11,37 @@
 
 // console.dir(document.querySelector(".oldImg"))
 
-//console.dir(document.querySelectorAll("div a"));
+// //console.dir(document.querySelectorAll("div a"));
 
-let links = document.querySelectorAll(".box a");
+//  let links = document.querySelectorAll(".box a");
 
-for (let i = 0; i < links.length; i++) {
-    links[i].style.color ="yellow";
-}
+// // for (let i = 0; i < links.length; i++) {
+// //     links[i].style.color ="yellow";
+// //}
+// for (link of links){
+//     link.style.color="red   "
+// }
+
+let para1 =document.createElement('p');
+para1.innerText="Hey iam red";
+document.querySelector("body").append(para1);
+
+para1.classList.add('red');
+
+let h3 =document.createElement('h3');
+h3.innerText="I am a blue h3!";
+document.querySelector("body").append(h3);
+
+// h3.classList.add("blue")
+
+let div=document.createElement('div');
+let h1 =document.createElement("h1");
+let para2=document.createElement('p');
+
+h1.innerText="i in a div";
+para2.innerText="Me too";
+div.append(h1);
+div.append(para2);
+div.classList.add("box");
+
+document.querySelector("body").append(div);
