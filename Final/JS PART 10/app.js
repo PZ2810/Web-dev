9cb5f903ef9c9bfa5124ea0e29f374b5944c9728
PZ2810 <121484876+PZ2810@users.noreply.github.com>
2025-09-09 23:06:00 +0530

    Add files via upload

--- Final/JS PART 10/app.js
+++ Final/JS PART 10/app.js
@@ -2,15 +2,24 @@ let btns=document.querySelectorAll("button");
 // console.dir(btn);
 
 for(btn of btns){
-    btn.onclick=sayhello;
+    // btn.addEventListener("click",sayhello);
+    // btn.addEventListener("click",sayName);
+     btn.addEventListener("dblclick",function(){
+        console.log("You can do it") ;
+     });
+    // btn.onclick=sayhello;
+    // btn.onclick=sayName;
    
-    btn.onmouseenter= function (){
-        console.log("hi");
-    };
-     console.dir(btn);
+    // btn.onmouseenter= function (){
+    //     console.log("hi");
+    // };
+    //  console.dir(btn);
 }
 function sayhello(){
 alert("hello!");
+}
+function sayName(){
+    alert ("Pranav ")
 }
 
 // btn.onclick = sayhello; 
