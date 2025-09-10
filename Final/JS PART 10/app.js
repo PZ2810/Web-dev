f40364d1e59dcf00c30e72e2835f7b55d41d019b
PZ2810 <121484876+PZ2810@users.noreply.github.com>
2025-09-10 23:56:19 +0530

    Add files via upload

--- Final/JS PART 10/app.js
+++ Final/JS PART 10/app.js
@@ -1,25 +1,76 @@
-let btns=document.querySelectorAll("button");
-// console.dir(btn);
+// let btns=document.querySelectorAll("button");
+// // console.dir(btn);
 
-for(btn of btns){
-    // btn.addEventListener("click",sayhello);
-    // btn.addEventListener("click",sayName);
-     btn.addEventListener("dblclick",function(){
-        console.log("You can do it") ;
-     });
-    // btn.onclick=sayhello;
-    // btn.onclick=sayName;
+// for(btn of btns){
+//     // btn.addEventListener("click",sayhello);
+//     // btn.addEventListener("click",sayName);
+//      btn.addEventListener("dblclick",function(){
+//         console.log("You can do it") ;
+//      });
+//     // btn.onclick=sayhello;
+//     // btn.onclick=sayName;
    
-    // btn.onmouseenter= function (){
-    //     console.log("hi");
-    // };
-    //  console.dir(btn);
-}
-function sayhello(){
-alert("hello!");
-}
-function sayName(){
-    alert ("Pranav ")
-}
+//     // btn.onmouseenter= function (){
+//     //     console.log("hi");
+//     // };
+//     //  console.dir(btn);
+// }
+// function sayhello(){
+// alert("hello!");
+// }
+// function sayName(){
+//     alert ("Pranav ")
+// }
 
-// btn.onclick = sayhello; 
+// // btn.onclick = sayhello; 
+
+// let p= document.querySelector("p");
+
+// p.addEventListener("click", function(){
+//     console.log("para was clicked");
+// })
+
+// let box=document.querySelector(".box");
+// box.addEventListener("mouseenter",function(){
+//     console.log("cursor inside the box")
+// })
+
+// let btn= document.querySelector("button");
+// let p =document.querySelector("p");
+// let h1=document.querySelector("h1");
+// let h3=document.querySelector("h3");
+
+
+// btn.addEventListener("click",function(){
+//     console.dir(this.innerText);
+//     this.style.backgroundColor="red";
+// });
+
+// p.addEventListener("click",function(){
+//     console.dir(this.innerText);
+//     this.style.backgroundColor="red";
+// });
+
+// h1.addEventListener("click",function(){
+//     console.dir(this.innerText);
+//     this.style.backgroundColor="red";
+// });
+
+// h3.addEventListener("click",function(){
+//     console.dir(this.innerText);
+//     this.style.backgroundColor="red";
+// });
+
+// function changeColor(){
+//     console.dir(this.innerText);
+//     this.style.backgroundColor="yellow"
+// }
+
+
+// btn.addEventListener("click",changeColor)
+  
+// p.addEventListener("click",changeColor);
+
+// h1.addEventListener("click",changeColor);
+
+// h3.addEventListener("click",changeColor);
