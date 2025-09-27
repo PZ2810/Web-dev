56c3c43a3f153363171475f8350c5c84660ec0aa
PZ2810 <121484876+PZ2810@users.noreply.github.com>
2025-09-28 00:48:17 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+// let n= 5;
+// for(let i=0;i<n;i++){
+//     console.log("hello", i);
+// }
+
+// console.log("bye!");
+
+// console.log(process.argv)
+
+// const math=require("./math");
+
+
+// console.log(math.sum(2,2));
+// console.log (math.PI)
+
+// const info= require("./fruits");
+
+// console.log(info[0].name);
+// import{sum,PI}from"./math.js";
+// console.log(sum(1,2));
+
+import { generate} from "random-words";
+console.log(generate());
